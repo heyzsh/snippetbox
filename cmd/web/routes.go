d9b0d11942da2aca7872a4c5c33bc1093691c685
Haidar Ali <haidarzsh@gmail.com>
2022-08-31 01:59:38 +0500

    restricted access to snippets

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,19 +19,19 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
-
-	// Snippet routes
+	// No protection
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
-	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.showSnippetCreate))
-	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.doSnippetCreate))
-
-	// Authentication Routes
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.showLogin))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.doLogin))
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.showSignup))
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.doSignup))
-	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.doSignout))
+
+	protected := dynamic.Append(app.requireAuthentication)
+	// Protected
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.doSignout))
+	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.showSnippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.doSnippetCreate))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	return standard.Then(router)
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -10,11 +10,12 @@ import (
 )
 
 type templateData struct {
-	CurrentYear int
-	Snippet     *models.Snippet
-	Snippets    []*models.Snippet
-	Form        any
-	Flash       string
+	CurrentYear     int
+	Snippet         *models.Snippet
+	Snippets        []*models.Snippet
+	Form            any
+	Flash           string
+	IsAuthenticated bool
 }
 
 var functions = template.FuncMap{
@@ -27,8 +28,9 @@ func humanDate(t time.Time) string {
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
