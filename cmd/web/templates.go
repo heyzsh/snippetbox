e31e8e0383168181870b10db539756503b4dd4c3
Haidar Ali <haidarzsh@gmail.com>
2022-08-31 19:21:47 +0500

    added csrf protection

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/justinas/nosurf"
 	"haidarz.com/internal/models"
 )
 
@@ -16,6 +17,7 @@ type templateData struct {
 	Form            any
 	Flash           string
 	IsAuthenticated bool
+	CSRFToken       string
 }
 
 var functions = template.FuncMap{
@@ -31,6 +33,7 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
